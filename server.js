0633696abd76fb0a3ff852e6539e99d4a8953b5d
amoee-5272 <62556646+amoee-5272@users.noreply.github.com>
2022-01-16 00:29:40 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ import express from 'express';
 import dotenv from 'dotenv-defaults';
 import mongoose from 'mongoose';
 import router from './backend/routes/index.js';
+import cors from "cors";
 dotenv.config();
 const app = express();
 //init middleware
+app.use(cors());
 app.use(express.json())
 /*app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
